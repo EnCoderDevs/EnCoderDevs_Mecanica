1b6d49ded2962ba94dc9b6c112ad06e55447f894
sherck001 <contactoseduardo01@gmail.com>
2024-08-23 23:55:36 -0400

    2-0

--- visual.js
+++ visual.js
@@ -1,19 +1,27 @@
-// scripts.js
-
 function mostrarFormulario(formulario) {
-    // Obtener todos los formularios
+    
     const formularios = document.querySelectorAll('form');
-    // Ocultar todos los formularios
+    
     formularios.forEach(form => form.classList.add('hidden'));
 
-    // Mostrar el formulario seleccionado
+  
     const formSeleccionado = document.getElementById(`${formulario}-form`);
     formSeleccionado.classList.remove('hidden');
 }
 
-// Asegúrate de que el script se cargue después del DOM
+
 document.addEventListener("DOMContentLoaded", function() {
-    // Inicialmente, ocultar todos los formularios
+
     const formularios = document.querySelectorAll('form');
     formularios.forEach(form => form.classList.add('hidden'));
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('.product button');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', function() {
+            alert('Product added to cart!');
+        });
+    });
 });
